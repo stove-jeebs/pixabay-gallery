2c3957f8e97bd594d5f60bc2cc76f66124e9b894
stove-jeebs <1876595827qq@gmail.com>
2022-06-07 15:13:59 +1200

    Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import React from "react";
-import Header from "./components/Header";
-import Content from "./components/Content";
+import React, { useState, useEffect, useRef } from "react";
+import Card from "./components/Card";
+import Search from "./components/Search";
 
-function App() {
+export default function Content() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photok`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
-    <div>
-      <Header />
-      <Content />
+    <div className="">
+      <Search />
+
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="container mx-auto my-8">
+          <div className="grid grid-cols-3 gap-5">
+            {images.map((image) => (
+              <Card key={image.id} image={image} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
