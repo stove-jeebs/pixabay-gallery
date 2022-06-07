ca144a9d998e5b0d6f18d45a45c73743b305583f
stove-jeebs <1876595827qq@gmail.com>
2022-06-07 12:35:34 +1200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {},
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
